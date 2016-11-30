9521f52de55b3663ab027cdfd06ad4a83cf0b3b0
silago <silago.nevermind@gmail.com>
2016-11-30 09:41:47 +0300

    swap

--- src/states/game.js
+++ src/states/game.js
@@ -6,51 +6,67 @@ class Game extends Phaser.State {
   }
 
   create() {
-    //var text = this.add.text(this.game.width * 0.5, this.game.height * 0.5, 'Game', {
-    //  font: '42px Arial', fill: '#ffffff', align: 'center'
-    //});
-    //text.anchor.set(0.5);
-    //
     this.grid = new jMatch3.Grid({
         width: 6,
         height: 7,
         gravity: "down"
     });
 
-    console.log(this.grid);
     this.types = [
             0,
             1,
             2
         ];
     
-    this.grid.forEachPiece((piece) => {
-        //console.log(piece);
-        var type_index = this.types[Math.floor(Math.random() * this.types.length)];
-        piece.clear = function() {
-            this.object.image.destroy();
-            this.object.type = "empty";// = null;//voidObject;
 
-        }
-        piece.object = {
-            'type': type_index, 
-            'image': this.game.add.sprite(piece.x*100,piece.y*100,'gems',type_index)
-        }
-        
-    });
-    this.grid.debug({
-        empty:'-',
-        0:'0',
-        1:'1',
-        2:'2'
-    });
+    this.prepareGrid = function() {
+        this.grid.forEachPiece((piece) => {
+            //console.log(piece);
+            var type_index = this.types[Math.floor(Math.random() * this.types.length)];
+            piece._clear = function() {
+                this.object.image.destroy();
+                this.object.image = null; 
+                this.clear();
+            }
+            piece.object = {
+                'type': type_index, 
+                'image': this.game.add.sprite(piece.x*100,piece.y*100,'gems',type_index)
+            }
 
+            piece.object.image.inputEnabled = true;
+            piece.object.image.events.onInputDown.add(
+              ()=> {this.swap(piece);}
+            , this);
+            
+        });
+    }
 
-    this.input.onDown.add(this.endGame, this);
+    this.swap_stack = [];
+    this.swap = function(i) {
+       this.swap_stack.push(i);
+       if (this.swap_stack.length<2) {
+         return;
+       }
+       this.grid.swapPieces(this.swap_stack[0],this.swap_stack[1]);
+       var updated = false;
+       this.grid.forEachMatch((m)=>{
+           updated = true;
+           m.forEach((i) => {
+            i._clear();
+           });
+       }); 
+       if (updated) {
+        this.grid.applyGravity();
+        this.prepareGrid();
+       }
+       this.swap_stack = [];
+    }
+    //this.input.onDown.add(this.endGame, this);
+
+    this.prepareGrid();
   }
 
   update() {
-
   }
 
   endGame() {
