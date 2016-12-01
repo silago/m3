66dcf3d5a131cde638a8d5b8a5d3e79b3478ddc6
Ovchinnikov Alexander <aovchinnikov@2reallife.com>
2016-12-01 12:15:39 +0300

    update heroku version

--- web.js
+++ web.js
@@ -3,11 +3,12 @@ var port = process.env.PORT || 5000;
 var app = express();
 
 app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
-}).configure(function() {
-    app.use('/assets', express.static(__dirname + '/assets'));
-    app.use('/node_modules/', express.static(__dirname + '/node_modules/'));
-    app.use('/src/', express.static(__dirname + '/src/'));
-}).listen(port, function() {
+    response.sendfile(__dirname + '/local.index.html');
+});
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/node_modules/', express.static(__dirname + '/node_modules/'));
+app.use('/src/', express.static(__dirname + '/src/'));
+
+app.listen(port, function() {
   console.log("Listening on " + port);
 });
